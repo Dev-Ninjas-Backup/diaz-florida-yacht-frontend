152589ef94846d7a4a217a701e72c8bbf7d429d2
habibb2r <habibhk127@gmail.com>
2025-11-05 03:22:01 +0600

    Fix: Lint fix

--- src/app/(main-layout)/layout.tsx
+++ src/app/(main-layout)/layout.tsx
@@ -1,15 +1,15 @@
-import AskAI from "@/components/shared/main/AskAI/AskAI";
-import Footer from "@/components/shared/main/Footer/Footer";
-import Navbar from "@/components/shared/main/Navbar/Navbar";
-import { ReactNode } from "react";
+import AskAI from '@/components/shared/main/AskAI/AskAI';
+import Footer from '@/components/shared/main/Footer/Footer';
+import Navbar from '@/components/shared/main/Navbar/Navbar';
+import { ReactNode } from 'react';
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="">
       <div className="my-2 md:my-3 mx-2 md:mx-5 rounded-2xl">
-         <Navbar />
-         <AskAI />
-      <div className="min-h-screen">{children}</div>
+        <Navbar />
+        <AskAI />
+        <div className="min-h-screen">{children}</div>
       </div>
       <Footer />
     </div>
@@ -17,4 +17,3 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
 };
 
 export default MainLayout;
-
