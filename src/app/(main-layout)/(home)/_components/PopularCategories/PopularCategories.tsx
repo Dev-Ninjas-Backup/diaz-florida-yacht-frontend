565727f0c045a5213e89c24a06c67b0482959ba3
Md-Ramjan-Ali <gmramjanali8888@gmail.com>
2025-12-26 06:40:30 +0600

    update

--- src/app/(main-layout)/(home)/_components/PopularCategories/PopularCategories.tsx
+++ src/app/(main-layout)/(home)/_components/PopularCategories/PopularCategories.tsx
@@ -1,15 +1,53 @@
-import { democategoriesdata } from '@/assets/demo-datas/demodata';
+'use client';
+
 import CustomContainer from '@/components/CustomComponents/CustomContainer';
+import React, { useEffect, useState } from 'react';
 import CategoryCard from './CategoryCard';
+import { getCategories } from '@/services/category/category';
+
+type CategoryUI = {
+  id: string;
+  name: string;
+  image: string;
+};
 
 const PopularCategories = () => {
+  const [categories, setCategories] = useState<CategoryUI[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      try {
+        const data = await getCategories();
+        const parsed: CategoryUI[] = data.map((item) => ({
+          id: item.id,
+          name: item.title,
+          image: item.image?.url ?? '',
+        }));
+
+        setCategories(parsed);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadCategories();
+  }, []);
+
+
+  if (loading) return <div className="">Loading ...</div>;
+  if (categories.length === 0) return <div className="">No data</div>;
+
   return (
     <CustomContainer>
-      <h2 className="text-xl sm:text-4xl lg:text-5xl text-center font-bold">
+      <h2 className="text-3xl sm:text-4xl lg:text-5xl text-center font-bold">
         Browse From Popular Categories
       </h2>
-      <div className="flex flex-wrap justify-center items-center gap-4 md:gap-10 my-5 md:my-10">
-        {democategoriesdata?.map((category) => (
+
+      <div className="flex flex-wrap justify-center items-center gap-10 my-10">
+        {categories.map((category) => (
           <CategoryCard key={category.id} category={category} />
         ))}
       </div>
